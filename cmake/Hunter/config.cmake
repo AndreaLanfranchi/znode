b36a0bddf291531276fd78f78d1115b32fa999a8
Andrea Lanfranchi <andrea.lanfranchi@gmail.com>
2023-07-16 16:47:47 +0200

    Bump Hunter -> 0.24.18

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -16,12 +16,8 @@ hunter_cmake_args(
         ABSL_RUN_TESTS=OFF
 )
 
-# Avoid -Werror to overcome GCC 12.1.0 bug breaking Google Benchmark build
-# https://github.com/google/benchmark/issues/1398
-# https://gcc.gnu.org/bugzilla/show_bug.cgi?id=105329
-hunter_config(
+hunter_cmake_args(
         benchmark
-        VERSION 1.6.1
         CMAKE_ARGS BENCHMARK_ENABLE_WERROR=OFF
 )
 
@@ -33,20 +29,13 @@ hunter_config(
 )
 
 hunter_config(
-        Boost
-        VERSION 1.81.0
-        URL https://boostorg.jfrog.io/artifactory/main/release/1.81.0/source/boost_1_81_0.tar.bz2
-        SHA1 898469f1ae407f5cbfca84f63ad602962eebf4cc
-)
-
-hunter_config(
         OpenSSL
-        VERSION 1.1.1t
-        URL https://github.com/openssl/openssl/archive/OpenSSL_1_1_1t.tar.gz
-        SHA1 34ea65451f7fc4625f31ba50f89b3fbea12f13f3
 
-        # See https://github.com/cpp-pm/hunter/pull/668
-        #VERSION 3.1.0
+#        VERSION 1.1.1t
+#        URL https://github.com/openssl/openssl/archive/OpenSSL_1_1_1t.tar.gz
+#        SHA1 34ea65451f7fc4625f31ba50f89b3fbea12f13f3
+
+        VERSION 3.1.0
         #URL https://github.com/openssl/openssl/archive/openssl-3.1.0.tar.gz
         #SHA1 1adb0f773af645b9f54738301920e5c74360b76d
 )
