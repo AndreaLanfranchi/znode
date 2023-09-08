56a400245e1f4e7cff578c31df7fe96824991917
Andrea Lanfranchi <andrea.lanfranchi@gmail.com>
2023-09-09 00:38:59 +0200

    Remove unused var

--- cmd/node.cpp
+++ cmd/node.cpp
@@ -149,7 +149,7 @@ int main(int argc, char* argv[]) {
         }
 
         // clang-tidy offers no way to suppress this warning
-        auto stop_asio{gsl::finally([&asio_context, &asio_guard, &asio_threads]() {
+        auto stop_asio{gsl::finally([&asio_guard, &asio_threads]() {
             log::Info("Service", {"name", "asio", "status", "stopping threads"});
             asio_guard.reset();  // Release the work guard - pending tasks will complete gracefully
             std::ranges::for_each(asio_threads, [](auto& thread) {
