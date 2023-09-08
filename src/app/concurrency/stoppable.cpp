f038e0181816e69d4abe17490d0e5ba8c905b69b
Andrea Lanfranchi <andrea.lanfranchi@gmail.com>
2023-09-08 15:40:58 +0200

    Clean-up unused import for Stoppable

--- src/app/concurrency/stoppable.cpp
+++ src/app/concurrency/stoppable.cpp
@@ -5,13 +5,11 @@
    file COPYING or http://www.opensource.org/licenses/mit-license.php.
 */
 
-#include <app/concurrency/ossignals.hpp>
 #include <app/concurrency/stoppable.hpp>
 
 namespace zenpp {
 
 bool Stoppable::start() noexcept {
-    if (Ossignals::signalled()) return false;  // Don't start if we're stopping
     bool expected{false};
     return started_.compare_exchange_strong(expected, true);
 }
@@ -21,7 +19,7 @@ bool Stoppable::stop([[maybe_unused]] /*in non-threaded components we don't need
     return stop_requested_.compare_exchange_strong(expected, true);
 }
 
-bool Stoppable::is_stopping() const noexcept { return stop_requested_ /*|| Ossignals::signalled()*/; }
+bool Stoppable::is_stopping() const noexcept { return stop_requested_; }
 
 bool Stoppable::is_running() const noexcept { return started_ and not stop_requested_; }
 
@@ -29,5 +27,4 @@ void Stoppable::set_stopped() noexcept {
     started_.store(false);
     stop_requested_.store(false);
 }
-
 }  // namespace zenpp
