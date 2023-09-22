92af9fba77de77a781ce272856845d13451444ea
Andrea Lanfranchi <andrea.lanfranchi@gmail.com>
2023-09-22 11:30:57 +0200

    Use concept

--- src/core/common/misc.hpp
+++ src/core/common/misc.hpp
@@ -40,7 +40,7 @@ namespace zenpp {
 
 //! \brief Parses a string representing an unsigned integer
 template <typename T>
-requires std::unsigned_integral<T>
+    requires std::unsigned_integral<T>
 bool try_parse_uint(std::string_view input, int base, T& output) noexcept {
     size_t pos{0};
     if (input.empty()) return false;
@@ -63,8 +63,8 @@ bool try_parse_uint(std::string_view input, int base, T& output) noexcept {
 bool try_parse_ip_address_and_port(std::string_view input, boost::asio::ip::address& address, uint16_t& port) noexcept;
 
 //! \brief Generates a random value of type T in a provided (min, max) range
-template <typename T>
-typename std::enable_if<std::is_integral<T>::value, T>::type randomize(const T min, const T max) {
+template <Integral T>
+T randomize(const T min, const T max) {
     ZEN_THREAD_LOCAL std::random_device rnd;
     ZEN_THREAD_LOCAL std::mt19937 gen(rnd());
     std::uniform_int_distribution<T> dis(min, max);
@@ -72,20 +72,20 @@ typename std::enable_if<std::is_integral<T>::value, T>::type randomize(const T m
 }
 
 //! \brief Generates a random value of type T in range (min,std::numeric_limits<T>::max())
-template <typename T>
-typename std::enable_if<std::is_integral<T>::value, T>::type randomize(const T min) {
+template <Integral T>
+T randomize(const T min) {
     return randomize<T>(static_cast<T>(min), std::numeric_limits<T>::max());
 }
 
 //! \brief Generates a random value of type T in range (std::numeric_limits<T>::max(),std::numeric_limits<T>::max())
-template <typename T>
-typename std::enable_if<std::is_integral<T>::value, T>::type randomize() {
+template <Integral T>
+T randomize() {
     return randomize<T>(std::numeric_limits<T>::min(), std::numeric_limits<T>::max());
 }
 
 //! \brief Generates a random value of type T in range (T * (1.0F - percentage), T * (1.0F + percentage))
-template <typename T>
-typename std::enable_if<std::is_integral<T>::value, T>::type randomize(T val, float percentage) {
+template <Integral T>
+T randomize(T val, float percentage) {
     percentage = std::abs(percentage);
     if (percentage > 1.0F) percentage = 1.0F;
     const T min = static_cast<T>(val * (1.0F - percentage));
