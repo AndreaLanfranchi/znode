beac5bfc410a56d297c931645546aa5c7a699b92
Andrea Lanfranchi <andrea.lanfranchi@gmail.com>
2023-10-05 16:59:57 +0200

    Timer coro

--- src/infra/concurrency/asio_timer.cpp
+++ src/infra/concurrency/asio_timer.cpp
@@ -1,8 +1,8 @@
 /*
-   Copyright 2022 The Silkworm Authors
-   Copyright 2023 Horizen Labs
-   Distributed under the MIT software license, see the accompanying
-   file COPYING or http://www.opensource.org/licenses/mit-license.php.
+Copyright 2022 The Silkworm Authors
+Copyright 2023 Horizen Labs
+Distributed under the MIT software license, see the accompanying
+file COPYING or http://www.opensource.org/licenses/mit-license.php.
 */
 
 #include "asio_timer.hpp"
@@ -11,63 +11,63 @@
 #include <utility>
 
 #include <absl/strings/str_cat.h>
+#include <boost/asio/co_spawn.hpp>
+#include <boost/asio/detached.hpp>
 
 #include <infra/common/log.hpp>
 
 namespace zenpp::con {
 bool AsioTimer::start() noexcept {
-    if (interval_milliseconds_ == 0U or not call_back_) return false;
-    if (Stoppable::start()) {
-        start_internal();
-        return true;
-    }
-    return false;
+ if (interval_milliseconds_ == 0U or not call_back_) return false;
+ if (Stoppable::start()) {
+     boost::asio::co_spawn(timer_.get_executor(), start_internal(), boost::asio::detached);
+     return true;
+ }
+ return false;
 }
 
 bool AsioTimer::start(uint32_t interval, CallBackFunc call_back) noexcept {
-    interval_milliseconds_.store(interval);
-    call_back_ = std::move(call_back);
-    return start();
+ interval_milliseconds_.store(interval);
+ call_back_ = std::move(call_back);
+ return start();
 }
 
 bool AsioTimer::stop(bool wait) noexcept {
-    if (Stoppable::stop(wait)) {
-        std::ignore = timer_.cancel();
-        if (wait) {
-            while (status() not_eq ComponentStatus::kNotStarted) {
-                std::this_thread::yield();
-                std::this_thread::sleep_for(std::chrono::milliseconds(100));
-            }
-        }
-        return true;
-    }
-    return false;
+ if (Stoppable::stop(wait)) {
+     std::ignore = timer_.cancel();
+     if (wait) {
+         while (status() not_eq ComponentStatus::kNotStarted) {
+             std::this_thread::yield();
+             std::this_thread::sleep_for(std::chrono::milliseconds(100));
+         }
+     }
+     return true;
+ }
+ return false;
 }
 
-void AsioTimer::start_internal() {
-    timer_.expires_after(std::chrono::milliseconds(interval_milliseconds_.load()));
-    timer_.async_wait([this](const boost::system::error_code& error_code) {
-        if (error_code) {
-            auto severity{error_code == boost::asio::error::operation_aborted ? log::Level::kTrace
-                                                                              : log::Level::kError};
-            log::BufferBase(severity, absl::StrCat("AsioTimer[", name_, "]"),
-                            {"action", "async_wait", "error", error_code.message()});
-            set_stopped();
-            return;
-        }
-        try {
-            const auto new_interval_milliseconds{call_back_(interval_milliseconds_)};
-            if (is_running() and autoreset() and new_interval_milliseconds > 0U) {
-                interval_milliseconds_.store(new_interval_milliseconds);
-                start_internal();
-            } else {
-                set_stopped();
-            }
-        } catch (const std::exception& ex) {
-            std::ignore =
-                log::Error(absl::StrCat("AsioTimer[", name_, "]"), {"action", "timer_expire", "error", ex.what()});
-            set_stopped();
-        }
-    });
+Task<void> AsioTimer::start_internal() noexcept {
+ try {
+     auto wait_interval = interval_milliseconds_.load();
+     while (is_running() and wait_interval not_eq 0U) {
+         timer_.expires_after(std::chrono::milliseconds(wait_interval));
+         co_await timer_.async_wait(boost::asio::use_awaitable);
+         wait_interval = call_back_(wait_interval);
+         if (not autoreset()) break;
+     }
+
+ } catch (const boost::system::system_error& error) {
+     if (error.code() not_eq boost::asio::error::operation_aborted) {
+         log::Error(absl::StrCat("AsioTimer[", name_, "]"),
+                    {"action", "async_wait", "error", error.code().message()});
+     }
+ } catch (const std::exception& exception) {
+     log::Critical(absl::StrCat("AsioTimer[", name_, "]"), {"action", "async_wait", "error", exception.what()});
+ } catch (...) {
+     log::Critical(absl::StrCat("AsioTimer[", name_, "]"), {"action", "async_wait", "error", "undefined"});
+ }
+
+ set_stopped();
+ co_return;
 }
 }  // namespace zenpp::con
--- src/infra/concurrency/asio_timer.hpp
+++ src/infra/concurrency/asio_timer.hpp
@@ -1,8 +1,8 @@
 /*
-   Copyright 2022 The Silkworm Authors
-   Copyright 2023 Horizen Labs
-   Distributed under the MIT software license, see the accompanying
-   file COPYING or http://www.opensource.org/licenses/mit-license.php.
+Copyright 2022 The Silkworm Authors
+Copyright 2023 Horizen Labs
+Distributed under the MIT software license, see the accompanying
+file COPYING or http://www.opensource.org/licenses/mit-license.php.
 */
 
 #pragma once
@@ -24,73 +24,74 @@
 #include <core/common/assert.hpp>
 
 #include <infra/concurrency/stoppable.hpp>
+#include <infra/concurrency/task.hpp>
 
 namespace zenpp::con {
 //! \brief Implementation of an asynchronous timer relying on boost:asio
 class AsioTimer : public Stoppable {
-  public:
-    //! \brief Callback function type to be executed when get_interval expires
-    //! \param interval [in] : length of wait get_interval (in milliseconds)
-    //! \return 0 if timer is to be stopped, otherwise the new get_interval (in milliseconds) between triggered events
-    using CallBackFunc = std::function<unsigned(unsigned)>;
+public:
+ //! \brief Callback function type to be executed when get_interval expires
+ //! \param interval [in] : length of wait get_interval (in milliseconds)
+ //! \return 0 if timer is to be stopped, otherwise the new get_interval (in milliseconds) between triggered events
+ using CallBackFunc = std::function<unsigned(unsigned)>;
 
-    //! \param asio_context [in] : boost's asio context
-    //! \param name [in] : name of the timer (for logging purposes)
-    AsioTimer(boost::asio::io_context& asio_context, std::string name) : timer_(asio_context), name_(std::move(name)){};
+ //! \param asio_context [in] : boost's asio context
+ //! \param name [in] : name of the timer (for logging purposes)
+ AsioTimer(boost::asio::io_context& asio_context, std::string name) : timer_(asio_context), name_(std::move(name)){};
 
-    //! \param asio_context [in] : boost's asio context
-    //! \param interval [in] : length of wait get_interval (in milliseconds)
-    //! \param name [in] : name of the timer (for logging purposes)
-    //! \param call_back [in] : the call back function to be executed when get_interval expires
-    AsioTimer(boost::asio::io_context& asio_context, uint32_t interval, std::string name, CallBackFunc call_back)
-        : timer_(asio_context),
-          interval_milliseconds_(interval),
-          name_(std::move(name)),
-          call_back_(std::move(call_back)){};
+ //! \param asio_context [in] : boost's asio context
+ //! \param interval [in] : length of wait get_interval (in milliseconds)
+ //! \param name [in] : name of the timer (for logging purposes)
+ //! \param call_back [in] : the call back function to be executed when get_interval expires
+ AsioTimer(boost::asio::io_context& asio_context, uint32_t interval, std::string name, CallBackFunc call_back)
+     : timer_(asio_context),
+       interval_milliseconds_(interval),
+       name_(std::move(name)),
+       call_back_(std::move(call_back)){};
 
-    ~AsioTimer() override = default;
+ ~AsioTimer() override = default;
 
-    //! \brief Returns the name of the timer
-    [[nodiscard]] const std::string& name() const noexcept { return name_; }
+ //! \brief Returns the name of the timer
+ [[nodiscard]] const std::string& name() const noexcept { return name_; }
 
-    //! \brief Returns the get_interval (in milliseconds) between triggered events
-    [[nodiscard]] uint32_t get_interval() const noexcept { return interval_milliseconds_.load(); }
+ //! \brief Returns the get_interval (in milliseconds) between triggered events
+ [[nodiscard]] uint32_t get_interval() const noexcept { return interval_milliseconds_.load(); }
 
-    //! \brief Sets the get_interval (in milliseconds) between triggered events
-    void set_interval(uint32_t interval_milliseconds) noexcept { interval_milliseconds_.store(interval_milliseconds); }
+ //! \brief Sets the get_interval (in milliseconds) between triggered events
+ void set_interval(uint32_t interval_milliseconds) noexcept { interval_milliseconds_.store(interval_milliseconds); }
 
-    //! \brief Returns true if timer is resubmitted after callback execution
-    [[nodiscard]] bool autoreset() const noexcept { return autoreset_.load(); }
+ //! \brief Returns true if timer is resubmitted after callback execution
+ [[nodiscard]] bool autoreset() const noexcept { return autoreset_.load(); }
 
-    //! \brief Sets the autoreset flag
-    void set_autoreset(bool value) noexcept { autoreset_.store(value); }
+ //! \brief Sets the autoreset flag
+ void set_autoreset(bool value) noexcept { autoreset_.store(value); }
 
-    //! \brief Sets the callback function to be executed when get_interval expires
-    void set_callback(CallBackFunc call_back) noexcept { call_back_ = std::move(call_back); }
+ //! \brief Sets the callback function to be executed when get_interval expires
+ void set_callback(CallBackFunc call_back) noexcept { call_back_ = std::move(call_back); }
 
-    //! \brief Starts timer and waits for get_interval to expire. Eventually call back action is executed and timer
-    //! resubmitted for another get_interval
-    bool start() noexcept override;
+ //! \brief Starts timer and waits for get_interval to expire. Eventually call back action is executed and timer
+ //! resubmitted for another get_interval
+ bool start() noexcept override;
 
-    //! \param interval_milliseconds [in] : length of wait get_interval (in milliseconds)
-    //! \param call_back [in] : the call back function to be executed when get_interval expires
-    bool start(uint32_t interval_milliseconds, CallBackFunc call_back) noexcept;
+ //! \param interval_milliseconds [in] : length of wait get_interval (in milliseconds)
+ //! \param call_back [in] : the call back function to be executed when get_interval expires
+ bool start(uint32_t interval_milliseconds, CallBackFunc call_back) noexcept;
 
-    //! \brief Stops timer and cancels pending execution. No callback is executed and no resubmission
-    bool stop([[maybe_unused]] bool wait) noexcept override;
+ //! \brief Stops timer and cancels pending execution. No callback is executed and no resubmission
+ bool stop([[maybe_unused]] bool wait) noexcept override;
 
-    //! \brief Cancels execution of awaiting callback and, if still in running state, submits timer for a new
-    //! get_interval
-    void reset() { std::ignore = timer_.cancel(); }
+ //! \brief Cancels execution of awaiting callback and, if still in running state, submits timer for a new
+ //! get_interval
+ void reset() { std::ignore = timer_.cancel(); }
 
-  private:
-    //! \brief Launches async timer
-    void start_internal();
+private:
+ //! \brief Launches async timer
+ Task<void> start_internal() noexcept;
 
-    boost::asio::steady_timer timer_;                // The timer itself
-    std::atomic_uint32_t interval_milliseconds_{0};  // Interval (in milliseconds) between triggered events
-    std::string name_{};                             // Name of the timer (for logging purposes)
-    std::atomic_bool autoreset_{true};               // If true, timer is resubmitted after callback execution
-    std::function<unsigned(unsigned)> call_back_;    // Function to call on triggered event
+ boost::asio::steady_timer timer_;                // The timer itself
+ std::atomic_uint32_t interval_milliseconds_{0};  // Interval (in milliseconds) between triggered events
+ std::string name_{};                             // Name of the timer (for logging purposes)
+ std::atomic_bool autoreset_{true};               // If true, timer is resubmitted after callback execution
+ std::function<unsigned(unsigned)> call_back_;    // Function to call on triggered event
 };
 }  // namespace zenpp::con
