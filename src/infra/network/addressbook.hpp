45243d3d947aa6abb6dd378933c8ec79f5979bee
Andrea Lanfranchi <andrea.lanfranchi@gmail.com>
2023-12-21 11:16:31 +0100

    AddressBook [WIP]
    Order of initialization

--- src/infra/network/addressbook.hpp
+++ src/infra/network/addressbook.hpp
@@ -61,7 +61,7 @@ class AddressBook {
     //! \brief A struct representing the coordinates of a slot in collection of buckets
     struct SlotAddress {
         SlotAddress() : x{0}, y{0} {};
-        explicit SlotAddress(uint16_t pos, uint16_t num) noexcept : x{pos}, y{num} {}
+        explicit SlotAddress(uint16_t pos, uint16_t num) noexcept : y{num}, x{pos} {}
         explicit SlotAddress(uint32_t pos_num) noexcept : xy{pos_num} {}
         union alignas(uint32_t) {
             struct alignas(uint16_t) {
